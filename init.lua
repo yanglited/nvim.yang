f89339581353602e9175928648bc48b914598764
Yang Li <yanglited@gmail.com>
2023-10-07 19:42:21 -0400

    wip: more keympas

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("yang.set")
 require("yang.core")
 require("yang.lazy")
+require("yang.keymaps")
---
+++ lua/yang/keymaps.lua
@@ -0,0 +1,30 @@
+
+-- center the cursor to middle of screen when scrolling up and down:
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- in normal and visual modes, yank into system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]]) -- yank to end of line, Y is equivalent to y$
+vim.keymap.set("n", "<leader>p", [["+p]]) -- paste from system clipboard
+
+-- use jk to exit insert mode
+vim.keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- delete single character without copying into register
+vim.keymap.set("n", "x", '"_x')
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
