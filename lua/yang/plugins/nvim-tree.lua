d9f501533f959502a046ecd96bd9c4658a923c52
Yang Li <yanglited@gmail.com>
2023-10-07 18:49:18 -0400

    Fixed up nvim-tree plugin keys and works well with which-key now

--- lua/yang/plugins/nvim-tree.lua
+++ lua/yang/plugins/nvim-tree.lua
@@ -1,7 +1,40 @@
 return {
     "nvim-tree/nvim-tree.lua",
+    lazy = false,
     dependencies = {
         "nvim-tree/nvim-web-devicons"
+    },
+    keys = {
+        {
+            "<leader>e",
+            function() require("nvim-tree") end,
+            desc = "File exploreer",
+        },
+        {
+            "<leader>ee",
+            "<cmd>NvimTreeFocus<CR>",
+            desc = "Focus on explore",
+        },
+        {
+            "<leader>ef",
+            "<cmd>NvimTreeFindFile<CR>",
+            desc = "Focus on current file",
+        },
+        {
+            "<leader>er",
+             "<cmd>NvimTreeRefresh<CR>",
+             desc = "Refresh file explorer",
+        },
+        {
+            "<leader>ec",
+            "<cmd>NvimTreeCollapse<CR>",
+            desc = "Collapse file explore",
+        },
+        {
+            "<leader>et",
+            "<cmd>NvimTreeToggle<CR>",
+            desc = "Toggle file explore",
+        },
     },
     config = function()
         local nvimtree = require("nvim-tree")
@@ -52,12 +85,5 @@ return {
             },
         })
 
-        -- set keymaps
-        local keymap = vim.keymap -- for conciseness
-
-        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
     end,
 }
