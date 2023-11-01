c9ef465d8849ba8ed7b72627bbfb8e5c202a514a
Yang Li <yanglited@gmail.com>
2023-11-01 10:45:25 -0400

    Adaptive view for nvim-tree

--- lua/yang/plugins/nvim-tree.lua
+++ lua/yang/plugins/nvim-tree.lua
@@ -52,6 +52,7 @@ return {
             view = {
                 width = 35,
                 relativenumber = true,
+                adaptive_size = true,
             },
             -- change folder arrow icons
             renderer = {
