0b29c2d06ea02d5c53341dfddce1bd7343a0e72c
Yang Li <yanlited@gmail.com>
2023-12-26 11:16:59 -0500

    Much improved shortcuts definition for telescope plugin

--- lua/yang/plugins/nvim-telescope.lua
+++ lua/yang/plugins/nvim-telescope.lua
@@ -3,41 +3,15 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim'
     },
-    keys = {
-        -- add a keymap to browse plugin files
-        -- stylua: ignore
-        {
-            "<leader>f",
-            desc = "Find things",
-        },
-        {
-            "<leader>fp",
-            function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-            desc = "Find Plugin File",
-        },
-        {
-            "<leader>ff",
-            function() require("telescope.builtin").find_files() end,
-            desc = "Find Files",
-        },
-        {
-            "<leader>fw",
-            function() require("telescope.builtin").live_grep() end,
-            desc = "Find Words (live grep)",
-        },
-        {
-            "<leader>fh",
-            function() require("telescope.builtin").help_tags() end,
-            desc = "Find Help Tags",
-        },
-    },
-    -- change some options
-    opts = {
-        defaults = {
-            layout_strategy = "horizontal",
-            layout_config = { prompt_position = "top" },
-            sorting_strategy = "ascending",
-            winblend = 0,
-        },
-    },
+    config = function()
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>f"] = {
+                name = "Find ...",
+                f = {":Telescope find_files<cr>", "Find files"},
+                w = {":Telescope live_grep<cr>",  "Live grep"},
+                h = {":Telescope help_tags<cr>",  "Find help tags"},
+            }
+        })
+    end,
 }
