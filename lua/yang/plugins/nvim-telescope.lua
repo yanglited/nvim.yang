dba42f1c2a4b7c774bb6b48279b265047320fc2a
Yang Li <yanlited@gmail.com>
2024-03-26 18:07:14 -0400

    find files and live grep to include hidden files

--- lua/yang/plugins/nvim-telescope.lua
+++ lua/yang/plugins/nvim-telescope.lua
@@ -1,17 +1,23 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-        'nvim-lua/plenary.nvim'
-    },
-    config = function()
-        local wk = require("which-key")
-        wk.register({
-            ["<leader>f"] = {
-                name = "Find ...",
-                f = {":Telescope find_files<cr>", "Find files"},
-                w = {":Telescope live_grep<cr>",  "Live grep"},
-                h = {":Telescope help_tags<cr>",  "Find help tags"},
-            }
-        })
-    end,
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("telescope").setup({
+			pickers = {
+				find_files = { hidden = true },
+				live_grep = { additional_args = {"--hidden"} },
+			},
+		})
+		local wk = require("which-key")
+		wk.register({
+			["<leader>f"] = {
+				name = "Find ...",
+				f = { ":Telescope find_files<cr>", "Find files" },
+				w = { ":Telescope live_grep<cr>", "Live grep"},
+				h = { ":Telescope help_tags<cr>", "Find help tags" },
+			},
+		})
+	end,
 }
