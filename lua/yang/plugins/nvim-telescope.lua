b0c9c7cf3f98dcae16d5d6c0e22e96c95f98e491
Yang Li <yanglited@gmail.com>
2024-04-05 22:38:57 -0400

    updated telescope

--- lua/yang/plugins/nvim-telescope.lua
+++ lua/yang/plugins/nvim-telescope.lua
@@ -1,23 +1,28 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		require("telescope").setup({
-			pickers = {
-				find_files = { hidden = true },
-				live_grep = { additional_args = {"--hidden"} },
-			},
-		})
-		local wk = require("which-key")
-		wk.register({
-			["<leader>f"] = {
-				name = "Find ...",
-				f = { ":Telescope find_files<cr>", "Find files" },
-				w = { ":Telescope live_grep<cr>", "Live grep"},
-				h = { ":Telescope help_tags<cr>", "Find help tags" },
-			},
-		})
-	end,
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        require("telescope").setup({
+            pickers = {
+                find_files = { hidden = true },
+                live_grep = { additional_args = { "--hidden" } },
+            },
+        })
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>f"] = {
+                name = "Find ...",
+                f = { ":Telescope find_files<cr>", "Find files" },
+                w = { ":Telescope live_grep<cr>", "Live grep" },
+                h = { ":Telescope help_tags<cr>", "Find help tags" },
+                g = { ":Telescope git_files<cr>", "Find git files" },
+                e = { ":Telescope help_tags<cr>", "Find help tags" },
+                r = { ":Telescope registers<cr>", "Find registers" },
+                b = { ":Telescope buffers<cr>", "Find buffers" },
+                z = { ":Telescope current_buffer_fuzzy_find<cr>", "Find fuzzy find in current buffer" },
+            },
+        })
+    end,
 }
