7cc6f5d1843a5888b8d628a8567880b7511c8031
Yang Li <yanlited@gmail.com>
2024-04-08 10:27:12 -0400

    added keyboard shortcuts for gitsigns

--- lua/yang/plugins/gitsigns.lua
+++ lua/yang/plugins/gitsigns.lua
@@ -1,5 +1,20 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    config = true,
+    -- config = true,
+    config = function()
+        local gitsigns = require("gitsigns")
+        gitsigns.setup()
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>G"] = {
+                name = "Gitsigns...",
+                n = { ":Gitsigns next_hunk<cr>", "Next hunk" },
+                p = { ":Gitsigns prev_hunk<cr>", "Prev hunk" },
+                b = { ":Gitsigns blame_line<cr>", "Blame line" },
+                d = { ":Gitsigns diffthis<cr>", "Diff this" },
+                r = { ":Gitsigns refresh<cr>", "Refresh" },
+            },
+        })
+    end,
 }
