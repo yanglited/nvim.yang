18983ee99f9fdb15f0a3520da3ed2d10bf945f01
Yang Li <yanglited@gmail.com>
2025-04-01 21:46:00 -0400

    fixing deprecated vim.fn.sign_define with vim.diagnostic.config

--- lua/yang/plugins/lsp-config.lua
+++ lua/yang/plugins/lsp-config.lua
@@ -100,11 +100,16 @@ return {
                 },
             })
 
-            local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-            end
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN]  = " ",
+                        [vim.diagnostic.severity.HINT]  = "󰠠 ",
+                        [vim.diagnostic.severity.INFO]  = " ",
+                    },
+                },
+            })
             lspconfig["bashls"].setup({
                 capabilities = capabilities,
                 on_attach = on_attach,
