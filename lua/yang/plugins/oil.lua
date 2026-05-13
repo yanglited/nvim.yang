2ae02ac7d6adeefd38b2d63563b59900fa73bbc8
Yang Li <yanglited@gmail.com>
2026-05-13 13:34:11 -0400

    wip: oil is not the default?

--- lua/yang/plugins/oil.lua
+++ lua/yang/plugins/oil.lua
@@ -2,7 +2,9 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		default_file_explorer = false,
+	},
 	-- Optional dependencies
 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
